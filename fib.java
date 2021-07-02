d5f33ba6914a2fbf3108a9a0616985c515bb3a32
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2021-07-02 12:44:34 -0700

    Add files via upload

---
+++ fib.java
@@ -0,0 +1,16 @@
+package Ex;
+
+public class fib {
+    public static int fib(int n){
+        if(n <= 2){
+            return 1;
+        }
+        else{
+            return fib(n-1) + fib(n-2);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(fib(5));
+//        Integer.parseInt()
+    }
+}
